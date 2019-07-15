3850781f1ced3867dfd5d1d8288177b93945e2aa
Eduardo <drdpedroso@gmail.com>
2019-07-15 15:56:26 -0300

    Add integration with feedy

--- .webpack/service/handler.js
+++ .webpack/service/handler.js
@@ -90,32 +90,75 @@
 /*!********************!*\
   !*** ./handler.js ***!
   \********************/
-/*! exports provided: helloWorld */
+/*! exports provided: getItems */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "helloWorld", function() { return helloWorld; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "getItems", function() { return getItems; });
 /* harmony import */ var source_map_support_register__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! source-map-support/register */ "source-map-support/register");
 /* harmony import */ var source_map_support_register__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(source_map_support_register__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var node_fetch__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! node-fetch */ "node-fetch");
+/* harmony import */ var node_fetch__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(node_fetch__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var date_fns__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! date-fns */ "date-fns");
+/* harmony import */ var date_fns__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(date_fns__WEBPACK_IMPORTED_MODULE_2__);
 
 
-const helloWorld = (event, context, callback) => {
+
+
+const mapItemsToNotionCard = (items = []) => {
+  return items.map(item => ({
+    title: item.title,
+    author: item.author,
+    date: Object(date_fns__WEBPACK_IMPORTED_MODULE_2__["distanceInWords"])(new Date(), new Date(item.published)),
+    url: item.originId
+  }));
+};
+
+const getItems = async (event, context, callback) => {
+  const getItemsUrl = 'https://cloud.feedly.com/v3/streams/contents?streamId=user%2F5f978fd3-1090-432c-bdd9-fec8395289c4%2Fcategory%2F2e702d82-a087-43fe-8694-d9598b01b104&count=40&unreadOnly=true&ranked=newest&similar=true&ck=1563208717329&ct=feedly.desktop&cv=31.0.405s';
+  const token = 'AxKS9tlc3li7-RNX9KjONhkZpkkJfrPb8tAHmcZ8Qg4vP6DFpw_EaM1c2Lgwknf2URRCR30VNp3mcOuVt0yPQJ4gxcXxOotFdATLAmq1ubOoqr5PZPEN_1nhuSZH4Jn-uFqd6tK2VcvlIG9t-LsStqF7tBsdeEW0DICYIC9fN2NX2hiR7_WPPVPC9NzskwYD7Vd5ks9tGV6KPfYBLpdjgUkyOAw58eutbV7gRMX3xOTxAfdBJ1JS1_xs-2K4dw:feedlydev';
+  const res = await node_fetch__WEBPACK_IMPORTED_MODULE_1___default()(getItemsUrl, {
+    method: 'get',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+  const jsonRes = await res.json();
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*' // Required for CORS support to work
 
     },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
-    })
+    body: JSON.stringify(mapItemsToNotionCard(jsonRes.items))
   };
-  callback(null, response);
+  return response;
 };
 
 
+
+/***/ }),
+
+/***/ "date-fns":
+/*!***************************!*\
+  !*** external "date-fns" ***!
+  \***************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("date-fns");
+
+/***/ }),
+
+/***/ "node-fetch":
+/*!*****************************!*\
+  !*** external "node-fetch" ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("node-fetch");
 
 /***/ }),
 
--- handler.js
+++ handler.js
@@ -1,17 +1,32 @@
+import fetch from 'node-fetch'
+import {distanceInWords} from 'date-fns'
 
-const helloWorld = (event, context, callback) => {
+const mapItemsToNotionCard = (items = []) => {
+  return items.map(item => ({
+    title: item.title,
+    author: item.author,
+    date: distanceInWords(new Date(), new Date(item.published)),
+    url: item.originId
+  }))
+};
+
+const getItems = async (event, context, callback) => {
+  const getItemsUrl = 'https://cloud.feedly.com/v3/streams/contents?streamId=user%2F5f978fd3-1090-432c-bdd9-fec8395289c4%2Fcategory%2F2e702d82-a087-43fe-8694-d9598b01b104&count=40&unreadOnly=true&ranked=newest&similar=true&ck=1563208717329&ct=feedly.desktop&cv=31.0.405s';
+  const token = 'AxKS9tlc3li7-RNX9KjONhkZpkkJfrPb8tAHmcZ8Qg4vP6DFpw_EaM1c2Lgwknf2URRCR30VNp3mcOuVt0yPQJ4gxcXxOotFdATLAmq1ubOoqr5PZPEN_1nhuSZH4Jn-uFqd6tK2VcvlIG9t-LsStqF7tBsdeEW0DICYIC9fN2NX2hiR7_WPPVPC9NzskwYD7Vd5ks9tGV6KPfYBLpdjgUkyOAw58eutbV7gRMX3xOTxAfdBJ1JS1_xs-2K4dw:feedlydev'
+  const res = await fetch(getItemsUrl, {
+    method: 'get',
+    headers: { 'Authorization': `Bearer ${token}` },
+  })
+  const jsonRes = await res.json()
   const response = {
     statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*', // Required for CORS support to work
     },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+    body: JSON.stringify(mapItemsToNotionCard(jsonRes.items)),
   };
 
-  callback(null, response);
+  return response
 };
 
-export { helloWorld }
+export { getItems }
